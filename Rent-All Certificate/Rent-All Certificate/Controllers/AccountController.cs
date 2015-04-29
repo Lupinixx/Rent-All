c7f192d67a563218fd409ce6aef97fb9ca57fe77
Wouter <wouter_1810@hotmail.com>
2015-04-29 11:10:33 +0200

    push alles

--- Rent-All Certificate/Rent-All Certificate/Controllers/AccountController.cs
+++ Rent-All Certificate/Rent-All Certificate/Controllers/AccountController.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Rent_All_Certificate.Controllers
 {
     [Authorize]
     public class AccountController : Controller
     {
-       
+        public ActionResult Login() {
+            return View();
+        }
     }
 }
