8637b840da6160a5c8d4e561d54ba732baee2dcd
Sebastiaan <sebastiaan_rutgers@hotmail.com>
2015-04-29 12:35:26 +0200

    Test push

--- Rent-All Certificate/Rent-All Certificate/Controllers/HomeController.cs
+++ Rent-All Certificate/Rent-All Certificate/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace Rent_All_Certificate.Controllers
 {
+    //.....Comment!
     public class HomeController : Controller
     {
         public ActionResult Index() {
