f9f8289898030dba4b8104140f4210c0e19d7d6e
Wouter <wouter_1810@hotmail.com>
2015-04-30 09:07:49 +0200

    Login Af

---
+++ Rent-All Certificate/Helpers/HashHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Helpers
+{
+    public class HashHelper
+    {
+        /// <summary>
+        /// Hash an Char-array with SHA512 and concat it with salt. 
+        /// </summary>
+        public string Hash(char[] toChar, string salt) {
+            var hasher = SHA512.Create();
+            var result = hasher.ComputeHash(Encoding.UTF8.GetBytes(string.Concat(new string(toChar), salt)));
+            return Convert.ToBase64String(result);
+        }
+    }
+}
---
+++ Rent-All Certificate/Rent-All Certificate/Controllers/LoginTestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rent_All_Certificate.Controllers
+{
+    public class LoginTestController : Controller
+    {
+        // GET: LoginTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
