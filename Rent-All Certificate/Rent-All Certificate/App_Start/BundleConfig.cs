0944b1864fbf2878de8c9307d7f43051a375e3ea
Luuk Maters <LJ.Maters@student.han.nl>
2015-04-30 13:12:21 +0200

    Dashboard - v0.2

--- Rent-All Certificate/Rent-All Certificate/App_Start/BundleConfig.cs
+++ Rent-All Certificate/Rent-All Certificate/App_Start/BundleConfig.cs
@@ -22,9 +22,25 @@ namespace Rent_All_Certificate
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/Content/jsdash").Include(
+                      "~/Content/dashboard/bower_components/jquery/dist/jquery.min.js",
+                      "~/Content/dashboard/bower_components/bootstrap/dist/js/bootstrap.min.js",
+                      "~/Content/dashboard/bower_components/metisMenu/dist/metisMenu.min.js",
+                      "~/Content/dashboard/bower_components/raphael/raphael-min.js",
+                      "~/Content/dashboard/bower_components/morrisjs/morris.min.js",
+                      "~/Content/dashboard/js/morris-data.js",
+                      "~/Content/dashboard/dist/js/sb-admin-2.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/cssdash").Include(
+                      "~/Content/dashboard/bower_components/bootstrap/dist/css/bootstrap.min.css",
+                      "~/Content/dashboard/bower_components/metisMenu/dist/metisMenu.min.css", 
+                      "~/Content/dashboard/dist/css/sb-admin-2.css",
+                      "~/Content/dashboard/bower_components/morrisjs/morris.css",
+                      "~/Content/dashboard/bower_components/font-awesome/css/font-awesome.min.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
