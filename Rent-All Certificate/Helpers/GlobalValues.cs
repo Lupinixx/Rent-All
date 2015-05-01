7b2828c4449ab31565715a01e0d324b4d056db3e
Luuk Maters <LJ.Maters@student.han.nl>
2015-05-01 10:36:32 +0200

    Laatste push naar master

--- Rent-All Certificate/Helpers/GlobalValues.cs
+++ Rent-All Certificate/Helpers/GlobalValues.cs
@@ -1,11 +1,10 @@
 ﻿namespace Helpers
 {
-    class GlobalValues
-    {
+
         public static class Roles
         {
             public const int TechnicalStaff = 1;
             public const int TechnicalAdministrator = 2;
         }
-    }
+    
 }
