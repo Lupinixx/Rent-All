4847b41c530a12476a84ab5f791e64f573373494
Luuk Maters <LJ.Maters@student.han.nl>
2015-05-11 12:07:08 +0200

    error solved metadatatype

--- Rent-All Certificate/Rent-All Certificate/Models/Employee.cs
+++ Rent-All Certificate/Rent-All Certificate/Models/Employee.cs
@@ -14,6 +14,7 @@ namespace Rent_All_Certificate.Models
 
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Employee
     {
