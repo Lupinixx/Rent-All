47bece5264a520e2ae9787c3901046cf846fe544
Sebastiaan <sebastiaan_rutgers@hotmail.com>
2015-05-11 14:10:10 +0200

    Changed Phasename row to Phase in the phase index

--- Rent-All Certificate/Rent-All Certificate/Models/Phase.cs
+++ Rent-All Certificate/Rent-All Certificate/Models/Phase.cs
@@ -9,31 +9,45 @@
 //------------------------------------------------------------------------------
 
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Rent_All_Certificate.Models
 {
 
-using System;
+    using System;
     using System.Collections.Generic;
-    
-public partial class Phase
-{
 
-    public Phase()
+    public partial class Phase
     {
 
-        this.Product = new HashSet<Product>();
+        public Phase()
+        {
+
+            this.Product = new HashSet<Product>();
+
+        }
+
+
+        public int PhaseID { get; set; }
+
+        public string PhaseName { get; set; }
+
+
+
+        public virtual ICollection<Product> Product { get; set; }
 
     }
 
+    [MetadataType(typeof(PhaseMetaData))]
+    public partial class Phase
+    {
+        
+    }
 
-    public int PhaseID { get; set; }
-
-    public string PhaseName { get; set; }
-
-
-
-    public virtual ICollection<Product> Product { get; set; }
-
-}
-
+    public class PhaseMetaData
+    {
+        [Display(Name = "Phase")]
+        [Required]
+        public string PhaseName { get; set; }
+    }
 }
