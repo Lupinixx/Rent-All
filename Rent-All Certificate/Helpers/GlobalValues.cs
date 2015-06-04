aa1f0cfd8c475589eb376e45d6f301b29d4aa004
Wouter <wouter_1810@hotmail.com>
2015-06-04 11:52:51 +0200

    Takel certificate upload and better error messages

--- Rent-All Certificate/Helpers/GlobalValues.cs
+++ Rent-All Certificate/Helpers/GlobalValues.cs
@@ -1,10 +1,14 @@
 ﻿namespace Helpers
 {
+    public static class Roles
+    {
+        public const int TechnicalStaff = 1;
+        public const int TechnicalAdministrator = 2;
+    }
 
-        public static class Roles
-        {
-            public const int TechnicalStaff = 1;
-            public const int TechnicalAdministrator = 2;
-        }
-
+    public static class CertificationTypes
+    {
+        public const int NEN3140 = 1;
+        public const int Hoist = 2;
+    }
 }
--- Rent-All Certificate/Rent-All Certificate/Controllers/CertificationsController.cs
+++ Rent-All Certificate/Rent-All Certificate/Controllers/CertificationsController.cs
@@ -14,7 +14,7 @@ namespace Rent_All_Certificate.Controllers
         private RentAllEntities db = new RentAllEntities();
 
         // GET: Certifications/Create
-        public ActionResult Index()
+        public ActionResult NEN3140()
         {
             ViewBag.BranchID = new SelectList(db.Branch, "BranchID", "BranchName");
             return View();
@@ -25,10 +25,11 @@ namespace Rent_All_Certificate.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Index(CertificationsUploadModel model)
+        public ActionResult NEN3140(CertificationsUploadModel model)
         {
             if (ModelState.IsValid)
             {
+                var fileSuccessCount = 0;
                 foreach (var file in model.Certificates)
                 {
                     if (file == null)
@@ -79,21 +80,118 @@ namespace Rent_All_Certificate.Controllers
                         }
 
                         var fileName = productKey + "-" + inventoryId + ".pdf";
-                        var path = Path.Combine(Server.MapPath("~/Content/Uploads"), fileName);
+                        var path = Path.Combine(Server.MapPath("~/Content/Uploads/NEN3140"), fileName);
                         file.SaveAs(path);
 
                         model.Certification.InventoryID = inventoryId;
+                        model.Certification.CertificateTypeID = CertificationTypes.NEN3140;
                         model.Certification.ProductKey = productKey;
                         model.Certification.EmployeeID = (int)Session["EmployeeID"];
                         model.Certification.Date = DateTime.Now;
                         db.Certification.Add(model.Certification);
                         db.SaveChanges();
+
+                        fileSuccessCount++;
                     }
                 }
+                ViewBag.FileSuccessCount = fileSuccessCount;
             }
             if (ModelState.IsValid)
             {
-                RedirectToAction("Index");
+                ViewBag.UploadMessage = "Your files have successfully been uploaded.";
+                RedirectToAction("NEN3140");
+            }
+            model.Certificates = null;
+            ViewBag.BranchID = new SelectList(db.Branch, "BranchID", "BranchName", model.Certification.BranchID);
+            return View(model);
+        }
+
+        public ActionResult Hoist()
+        {
+            ViewBag.BranchID = new SelectList(db.Branch, "BranchID", "BranchName");
+            return View();
+        }
+
+        // POST: Certifications/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Hoist(CertificationsUploadModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var fileSuccessCount = 0;
+                foreach (var file in model.Certificates)
+                {
+                    if (file == null)
+                    {
+                        continue;
+                    }
+                    if (file.ContentType != "application/pdf")
+                    {
+                        ModelState.AddModelError("", "The file :\"" + file.FileName + "\" isn't recognized as a pdf file.");
+                        continue;
+                    }
+                    var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(file.FileName);
+                    if (fileNameWithoutExtension != null)
+                    {
+                        var keys = fileNameWithoutExtension.Split('-');
+
+                        if (keys.Count() != 2)
+                        {
+                            ModelState.AddModelError("", "The file :\"" + file.FileName + "\" has an incorrect file name.");
+                            continue;
+                        }
+
+                        int inventoryId;
+                        var productKey = keys[0];
+
+                        if (db.Product.Any(p => p.ProductKey == productKey) == false)
+                        {
+                            ModelState.AddModelError("", "The key :\"" + productKey + "\" was not found.");
+                            continue;
+                        }
+
+                        bool parsed = Int32.TryParse(keys[1], out inventoryId);
+
+                        if (parsed == false)
+                        {
+                            ModelState.AddModelError("", "The inventory id from file :\"" + file.FileName + "\" is not a number.");
+                            continue;
+                        }
+
+                        if (db.Inventory.Any(i => i.ProductKey == productKey && i.InventoryID == inventoryId) == false)
+                        {
+                            db.Inventory.Add(new Inventory
+                            {
+                                InventoryID = inventoryId,
+                                ProductKey = productKey
+                            });
+                            db.SaveChanges();
+                        }
+
+                        var fileName = productKey + "-" + inventoryId + ".pdf";
+                        var path = Path.Combine(Server.MapPath("~/Content/Uploads/Hoist"), fileName);
+                        file.SaveAs(path);
+
+                        model.Certification.InventoryID = inventoryId;
+                        model.Certification.ProductKey = productKey;
+                        model.Certification.CertificateTypeID = CertificationTypes.Hoist;
+                        model.Certification.EmployeeID = (int)Session["EmployeeID"];
+                        model.Certification.Date = DateTime.Now;
+                        db.Certification.Add(model.Certification);
+                        db.SaveChanges();
+
+                        fileSuccessCount++;
+                    }
+                }
+                ViewBag.FileSuccessCount = fileSuccessCount;
+            }
+            if (ModelState.IsValid)
+            {
+                ViewBag.UploadMessage = "Your files have successfully been uploaded.";
+                RedirectToAction("Hoist");
             }
             model.Certificates = null;
             ViewBag.BranchID = new SelectList(db.Branch, "BranchID", "BranchName", model.Certification.BranchID);
